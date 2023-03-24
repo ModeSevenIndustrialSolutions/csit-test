7ee7eae100df635f31f07e31f7b813d9064c126f
Matthew Watkins <mwatkins@linuxfoundation.org>
2023-03-24 14:02:18 +0000

    minor fixes

--- jjb/integration/run-csit.sh
+++ jjb/integration/run-csit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 #
 # Copyright 2016-2017 Huawei Technologies Co., Ltd.
-# Modification Copyright 2019 Samsung Electronics Co., Ltd.
+# Modification Copyright 2019 © Samsung Electronics Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,16 +15,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# $1 project/functionality {TESTPLAN}
-# $2 robot options         {TESTOPTIONS}
-
-echo "---> run-csit.sh"
+# $1 project/functionality
+# $2 robot options
 
 #
 # functions
 #
 
-function on_exit {
+function on_exit(){
     rc=$?
     if [[ ${WORKSPACE} ]]; then
         if [[ ${WORKDIR} ]]; then
@@ -39,64 +37,17 @@ function on_exit {
     # Run teardown script plan if it exists
     cd "${TESTPLANDIR}"
     TEARDOWN="${TESTPLANDIR}/teardown.sh"
-    if [[ -f "${TEARDOWN}" ]]; then
+    if [ -f "${TEARDOWN}" ]; then
         echo "Running teardown script ${TEARDOWN}"
         source_safely "${TEARDOWN}"
     fi
-    exit $rc
+    # TODO: do something with the output
+     exit $rc
 }
 # ensure that teardown and other finalizing steps are always executed
 trap on_exit EXIT
 
-# load the saved set options
-function load_set {
-    _setopts="$-"
-
-    # bash shellopts
-    for i in $(echo "$SHELLOPTS" | tr ':' ' ') ; do
-        set +o "${i}"
-    done
-    for i in $(echo "$RUN_CSIT_SHELLOPTS" | tr ':' ' ') ; do
-        set -o "${i}"
-    done
-
-    # other options
-    for i in $(echo "$_setopts" | sed 's/./& /g') ; do
-        set +"${i}"
-    done
-    set -"${RUN_CSIT_SAVE_SET}"
-}
-
-# set options for quick bailout when error
-function harden_set {
-    set -xeo pipefail
-    set +u # enabled it would probably fail too many often
-}
-
-# relax set options so the sourced file will not fail
-# the responsibility is shifted to the sourced file...
-function relax_set {
-    set +e
-    set +o pipefail
-}
-
-# save current set options
-function save_set {
-    RUN_CSIT_SAVE_SET="$-"
-    RUN_CSIT_SHELLOPTS="$SHELLOPTS"
-}
-
-# wrapper for sourcing a file
-function source_safely {
-    if [[ -z "$1" ]] && return 1; then
-        relax_set
-        # shellcheck disable=SC1090
-        source "$1"
-        load_set
-    fi
-}
-
-function docker_stats {
+function docker_stats(){
     #General memory details
     echo "> top -bn1 | head -3"
     top -bn1 | head -3
@@ -114,6 +65,52 @@ function docker_stats {
     echo "> docker stats --no-stream"
     docker stats --no-stream
     echo
+}
+
+# save current set options
+function save_set() {
+    RUN_CSIT_SAVE_SET="$-"
+    RUN_CSIT_SHELLOPTS="$SHELLOPTS"
+}
+
+# load the saved set options
+function load_set() {
+    _setopts="$-"
+
+    # bash shellopts
+    for i in $(echo "$SHELLOPTS" | tr ':' ' ') ; do
+        set +o ${i}
+    done
+    for i in $(echo "$RUN_CSIT_SHELLOPTS" | tr ':' ' ') ; do
+        set -o ${i}
+    done
+
+    # other options
+    for i in $(echo "$_setopts" | sed 's/./& /g') ; do
+        set +${i}
+    done
+    set -${RUN_CSIT_SAVE_SET}
+}
+
+# set options for quick bailout when error
+function harden_set() {
+    set -xeo pipefail
+    set +u # enabled it would probably fail too many often
+}
+
+# relax set options so the sourced file will not fail
+# the responsibility is shifted to the sourced file...
+function relax_set() {
+    set +e
+    set +o pipefail
+}
+
+# wrapper for sourcing a file
+function source_safely() {
+    [ -z "$1" ] && return 1
+    relax_set
+    . "$1"
+    load_set
 }
 
 #
@@ -134,14 +131,8 @@ then
     exit 1
 fi
 
-if [[ -z "${WORKSPACE}" ]]; then
-    if (git rev-parse --show-toplevel > /dev/null 2>&1); then
-        WORKSPACE=$(git rev-parse --show-toplevel)
-        export WORKSPACE
-    else
-        WORKSPACE=$(pwd)
-        export WORKSPACE
-    fi
+if [ -z "$WORKSPACE" ]; then
+    export WORKSPACE=$(git rev-parse --show-toplevel)
 fi
 
 if [ -f "${WORKSPACE}/${1}/testplan.txt" ]; then
@@ -158,40 +149,11 @@ mkdir -p "$WORKSPACE/archives/$TESTPLAN"
 
 TESTPLANDIR="${WORKSPACE}/${TESTPLAN}"
 
-# Run installation of required libraries
+# Run installation of prerequired libraries
 source_safely "${WORKSPACE}/prepare-csit.sh"
 
-# Python version should match that used to setup
-#  robot-framework in other jobs/stages
-# Use pyenv for selecting the python version
-if [[ -d "/opt/pyenv" ]]; then
-    echo "Setup pyenv:"
-    export PYENV_ROOT="/opt/pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
-    pyenv versions
-    if command -v pyenv 1>/dev/null 2>&1; then
-        eval "$(pyenv init - --no-rehash)"
-        # Choose the latest numeric Python version from installed list
-        version=$(pyenv versions --bare \
-            | sed '/^[^0-9]/d' | sort -V | tail -n 1)
-        pyenv local "${version}"
-    fi
-fi
-
 # Activate the virtualenv containing all the required libraries installed by prepare-csit.sh
-# source_safely "${ROBOT3_VENV}/bin/activate"
-
-# Assume that if ROBOT3_VENV is set, virtualenv
-#  with system site packages can be activated
-if [[ -f "${WORKSPACE}/env.properties" ]]; then
-    source "${WORKSPACE}/env.properties"
-elif [[ -f /tmp/env.properties ]]; then
-    source /tmp/env.properties
-fi
-
-if [[ -f "${ROBOT3_VENV}/bin/activate" ]]; then
-    source "${ROBOT3_VENV}/bin/activate"
-fi
+source_safely "${ROBOT3_VENV}/bin/activate"
 
 WORKDIR=$(mktemp -d --suffix=-robot-workdir)
 cd "${WORKDIR}"
@@ -225,13 +187,7 @@ SUITES=$( xargs -a testplan.txt )
 echo ROBOT_VARIABLES="${ROBOT_VARIABLES}"
 echo "Starting Robot test suites ${SUITES} ..."
 relax_set
-
-echo "Versioning information:"
-python3 --version
-pip3 freeze
-python3 -m robot.run --version || :
-
-python3 -m robot.run -N "${TESTPLAN}" -v WORKSPACE:/tmp "${ROBOT_VARIABLES}" "${TESTOPTIONS}" "${SUITES}"
+python3 -m robot.run -N ${TESTPLAN} -v WORKSPACE:/tmp ${ROBOT_VARIABLES} ${TESTOPTIONS} ${SUITES}
 RESULT=$?
 load_set
 echo "RESULT: $RESULT"
